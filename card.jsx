d5b6c8cf10710d76b67e54f7a5d8ddda4ea670bb
aivandag <aivan.dev7@gmail.com>
2025-05-29 01:59:02 +0000

    starwars the phatom error

--- card.jsx
+++ card.jsx
@@ -6,7 +6,18 @@ const { store, dispatch } = useGlobalReducer();
 const navigate = useNavigate();
 
 const favorite = store.favorite.some(
-    fav = fav.uid == isHtmlElement.uid && fav.type ==type
+    fav = fav.uid == isHtmlElement.uid && fav.type == type
 );
 
-const imgUrl = 
+const imgUrl = `https://starwars-visualguide.com/assets/img/${type === 'people' ? 'characters' : type}/${item.uid}.jpg`;
+
+const handleFavorite = ()  = {
+if ()
+    dispatch({ type: "remove_favorite", payload: { uid: item.uid, type } });
+
+} else {
+    dispatch({
+        type: "add_favorite",
+        payload: {}
+    })
+}
